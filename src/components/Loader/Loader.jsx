d4b7c00a9194821ef7cb68ee0522b73f129ee61b
TempUsername2436 <c.benton.145@gmail.com>
2022-05-28 11:26:16 -0700

    Fixed pointer events on the loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,9 +4,13 @@ import './Loader.css';
 function Loader({ duration, delay }) {
   useEffect(() => {
     const html = document.querySelector('html');
+    const loader = document.querySelector('.loader');
     html.classList.add('static');
 
-    setTimeout(() => html.classList.remove('static'), Number(delay));
+    setTimeout(() => {
+      html.classList.remove('static');
+      loader.style.pointerEvents = 'none';
+    }, Number(delay));
   });
 
   return <div className='loader' style={{ '--duration': `${duration}ms`, '--delay': `${delay}ms` }}></div>;
